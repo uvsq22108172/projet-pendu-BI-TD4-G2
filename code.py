3398125be4de3accdbe04e9a9e179e9f77bc1e12
uvsq22108172 <113902778+uvsq22108172@users.noreply.github.com>
2023-03-24 12:55:44 +0100

    Add files via upload

--- code.py
+++ code.py
@@ -1,47 +1,64 @@
-from random import choice
+# random:le choix aleatoir d'un element à partir d'une liste
+# unidecode: retourner une chaîne de caractères sans accents
+from numpy import random
 from unidecode import unidecode
 
-# un mot au hasard
+# importer le dictionnaire de mots francais
+liste_de_mots_francais = []
+fichier_de_mots_francais = open('liste_fracais.txt', 'rt')
+for m in fichier_de_mots_francais:
+    liste_de_mots_francais.append(m.strip("\n"))
+
+mot = liste_de_mots_francais[random.randint(0, len(liste_de_mots_francais)-1)]
 
 
-def word():
-    f = open('mots.txt', 'r' , encoding = 'utf8')
-    contenu = f.readlines()
-    return unidecode( choice(contenu) ).upper().replace('\n','')
-
-# remplacement par des underscores
+# choisir la longueur du mot
 
 
-def underscore(mot , L = []):
-    r = ''
-    for i in mot:
-        if i in L:
-            r += i + ' '
-        else:
-            r += '* '
-            
+def longueur_du_mot(l,mot):
+   l = int(input("choisir le nombre de lettre"))
+   mot = liste_de_mots_francais[random.randint(0, len(liste_de_mots_francais)-1)]
+   while len(mot) != l:
+      mot = liste_de_mots_francais[random.randint(0, len(liste_de_mots_francais)-1)]
+    return mot
+      
+
+# asterix: remplace les lettres par des *
+
+
+def asterix(mot):
+    r = '* ' * len(mot)
     return r[:-1]
 
-# saisie d'une lettre
 
-
+# upper:retourner une chaîne de caractères en majuscules
+# le “65” correspond au code ASCII de “A” et le “122” à celui de “z”
 def saisie():
     lettre = input('Entrez une lettre : ')
-    if len( lettre ) > 1 or ord(lettre) < 65 or ord(lettre) > 122:
+    if len(lettre) > 1 or ord(lettre) < 65 or ord(lettre) > 122:
         return saisie()
     else:
         return lettre.upper()
-    
 
-# programme principal
+
+def word():
+    f = open('liste_fracais.txt', 'rt')
+    contenu = f.readlines()
+    return unidecode( choice(contenu) ).upper().replace('\n','')
+
+
 
 lettres_deja_proposees = []
+
 mot_a_deviner = word()
-affichage = underscore( mot_a_deviner )
+
+affichage = asterix( mot_a_deviner )
+
 print( 'Mot à deviner : ' , affichage )
+
 nb_erreurs = 0
 
-while '_' in affichage and nb_erreurs < 11:
+while '*' in affichage and nb_erreurs < 11:
     lettre = saisie()
     if lettre not in lettres_deja_proposees:
         lettres_deja_proposees += [ lettre ]
@@ -49,5 +66,5 @@ while '_' in affichage and nb_erreurs < 11:
     if lettre not in mot_a_deviner:
         nb_erreurs += 1
             
-    affichage = underscore( mot_a_deviner , lettres_deja_proposees )
+    affichage = asterix( mot_a_deviner , lettres_deja_proposees )
     print( '\nMot à deviner : ' , affichage , ' '*10 , 'Nombre d\'erreurs maximum :' , 11-nb_erreurs )
