90b726e9c767d01865e927f2e8afb6927ab40e92
uvsq22108172 <113902778+uvsq22108172@users.noreply.github.com>
2023-04-03 23:12:22 +0200

    Add files via upload

--- code.py
+++ code.py
@@ -1,70 +1,31 @@
-# random:le choix aleatoir d'un element à partir d'une liste
-# unidecode: retourner une chaîne de caractères sans accents
-from numpy import random
-from unidecode import unidecode
+# code du jeu pendu
+import random
 
-# importer le dictionnaire de mots francais
-liste_de_mots_francais = []
-fichier_de_mots_francais = open('liste_fracais.txt', 'rt')
-for m in fichier_de_mots_francais:
-    liste_de_mots_francais.append(m.strip("\n"))
+# liste de listes de mots avec nombre de lettres croissant 0 , 1, 2, 3, 5...
+L = [[], [], [], [], [], ['ACTIF','AVENU'], ['WHISKY', 'COCCYX'], ['ACCOLAT', 'ACCOLEE'],['ABOUTIES','ZOOLOGIE'], ['ABDOMINAL','VAISSELLE']]
 
-mot = liste_de_mots_francais[random.randint(0, len(liste_de_mots_francais)-1)]
+# CHOIX de longueur du mot
+longueur_de_mot = int(input("nobre de lettre"))
 
+# Le choix aleatoire du mot avec nombre de lettres predefini
+mot_a_deviner = random(L[longueur_de_mot])
 
-# choisir la longueur duJJJJJJ
+# Cacher le mot à deviner par des astérix
+asterix = '* ' * len(mot_a_deviner)
+print(asterix)
 
+# Choix du nombres de tentatives autorisées
+tentatives = int(input('nobres de tentatives'))
+erreurs = 0
 
-def longueur_du_mot(l,mot):
-   l = int(input("choisir le nombre de lettre"))
-   mot = liste_de_mots_francais[random.randint(0, len(liste_de_mots_francais)-1)]
-   while len(mot) != l:
-      mot = liste_de_mots_francais[random.randint(0, len(liste_de_mots_francais)-1)]
-    return mot
-      
+while asterix != mot_a_deviner and erreurs < tentatives:
+    lettre = input("Entrez une lettre : ")
+    for i in range(len(mot_a_deviner)):
+        if lettre == mot_a_deviner[i]:
+            asterix = asterix[:i] + lettre + asterix[i+1:]
+    print(asterix)
 
-# asterix: remplace les lettres par des *
-
-
-def asterix(mot):
-    r = '* ' * len(mot)
-    return r[:-1]
-
-
-# upper:retourner une chaîne de caractères en majuscules
-# le “65” correspond au code ASCII de “A” et le “122” à celui de “z”
-def saisie():
-    lettre = input('Entrez une lettre : ')
-    if len(lettre) > 1 or ord(lettre) < 65 or ord(lettre) > 122:
-        return saisie()
-    else:
-        return lettre.upper()
-
-
-def word():
-    f = open('liste_fracais.txt', 'rt')
-    contenu = f.readlines()
-    return unidecode( choice(contenu) ).upper().replace('\n','')
-
-
-
-lettres_deja_proposees = []
-
-mot_a_deviner = word()
-
-affichage = asterix( mot_a_deviner )
-
-print( 'Mot à deviner : ' , affichage )
-
-nb_erreurs = 0
-
-while '*' in affichage and nb_erreurs < 11:
-    lettre = saisie()
-    if lettre not in lettres_deja_proposees:
-        lettres_deja_proposees += [ lettre ]
-        
-    if lettre not in mot_a_deviner:
-        nb_erreurs += 1
-            
-    affichage = asterix( mot_a_deviner , lettres_deja_proposees )
-    print( '\nMot à deviner : ' , affichage , ' '*10 , 'Nombre d\'erreurs maximum :' , 11-nb_erreurs )
+if mot_a_deviner == asterix and erreurs < tentatives:
+    print('Bravo ! Le mot', mot_a_deviner, 'a été trouvé')
+else:
+    print('vous avez perdu! Le mot', mot_a_deviner, 'n a pas etait retrouvé')
