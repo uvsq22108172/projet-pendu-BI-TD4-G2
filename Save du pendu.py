7084bfc13ee5c7146130adbf584325870ec1d928
uvsq22202673 <113599377+uvsq22202673@users.noreply.github.com>
2023-04-26 20:34:41 +0200

    Images

--- Save du pendu.py
+++ Save du pendu.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import *
 import random
 import pygame
+from PIL import Image,ImageTk
 
 graphique = tk.Tk()                                   
 graphique.geometry("1920x1080")                                 
@@ -10,9 +11,9 @@ graphique.title("Pendu BI-TD4_Groupe 2")
 mainmenu = tkinter.Menu(graphique)
 
 label = Label(text= "Jeu du Pendu")                                     
-label.pack(pady=100) 
+label.pack(pady=100)
 
-aide1 = None       #variable globale 
+aide1 = None
 
 def aide_pendu() :
     global aide1  #référence variable globale
@@ -42,7 +43,7 @@ entier_menu = tk.OptionMenu(graphique, entier_var, 6, 7, 8, 9, 10)
 entier_menu.pack()
 
 # Création d'un bouton pour valider le choix du nombre de lettres
-choisir_entier_btn = tk.Button(graphique, text="Confirmer le nb de lettres", command=word_length)
+choisir_entier_btn = tk.Button(graphique, text="Confirmer le nombre de lettres", command=word_length)
 choisir_entier_btn.pack()
 
 # Création d'un label pour afficher la valeur choisie
@@ -64,7 +65,7 @@ number_menu = tk.OptionMenu(graphique, number_var, 5, 7, 9)
 number_menu.pack()
 
 # Création d'un bouton pour valider le choix de l'entier
-choisir_number_btn = tk.Button(graphique, text="Confirmer le nb de tentatives", command=attempts_allowed)
+choisir_number_btn = tk.Button(graphique, text="Confirmer le nombre de tentatives", command=attempts_allowed)
 choisir_number_btn.pack()
 
 # Création d'un label pour afficher la valeur choisie
@@ -73,9 +74,8 @@ val_label.pack()
 
 
 def jeu_du_pendu() :
-    # sources : http://lycee.lagrave.free.fr/isn/projet/8_projet_pendu_GUI_tkinter.html, https://www.mathweb.fr/euclide/2020/09/07/le-jeu-du-pendu-en-python/
+    #sources : http://lycee.lagrave.free.fr/isn/projet/8_projet_pendu_GUI_tkinter.html, https://www.mathweb.fr/euclide/2020/09/07/le-jeu-du-pendu-en-python/
                 
-    # Fonction pour choisir un mot au hasard
 
     #images = ["C:\Fac\L1 Semestre 2\IN200N\projet-pendu-BI-TD4-G2-main\Dessin pendu\erreur1", #images du pendu
      #"C:\Fac\L1 Semestre 2\IN200N\projet-pendu-BI-TD4-G2-main\Dessin pendu\erreur2", 
@@ -84,18 +84,18 @@ def jeu_du_pendu() :
     #"C:\Fac\L1 Semestre 2\IN200N\projet-pendu-BI-TD4-G2-main\Dessin pendu\erreur5", 
     #"C:\Fac\L1 Semestre 2\IN200N\projet-pendu-BI-TD4-G2-main\Dessin pendu\erreur6", 
     #"C:\Fac\L1 Semestre 2\IN200N\projet-pendu-BI-TD4-G2-main\Dessin pendu\erreur7"]
-
+    
     def choix_du_mot() :
         mots = [['touche', 'joyeux'], ['docteur', 'ajdoint'], ['biologie', 'absorber'], ['abdominal', 'zymologie'], ['architecte', 'professeur']]
         return random.choice(mots[entier-6])
 
-    def vérification(lettre, mot, mot_caché) :           #vérifie si la lettre est dans le mot
-        for i in range(len(mot)) :
-            if mot[i] == lettre :                        
-                mot_caché = mot_caché[:i] + lettre + mot_caché[i+1:]
-        return mot_caché
+    def vérification(lettre, mot, mot_caché) :                              #vérifie si la lettre est dans le mot
+        for i in range(len(mot)) :                                          #parcourt tout le mot selon son nombre de lettre grace a "len"
+            if mot[i] == lettre :                                           #vérifie si la lettre entrée est la meme que la lettre actuelle dans la boucle
+                mot_caché = mot_caché[:i] + lettre + mot_caché[i+1:]        #met a jour la variable mot_caché pour remplacer la lettre correspondante a la lettre donnée, remplace  la lettre à l'index i par la lettre donnée, on obtient une nouvelle chaîne de caractères affectée à la variable "mot_caché"
+        return mot_caché                                                    #renvoie la variable mot_caché qui est mise a jour, contient le mot caché avec la lettre donnée remplacée si elle était présente
 
-    def montrer_mot_caché(mot_caché) :             #affiche le mot caché
+    def montrer_mot_caché(mot_caché) :             #prend mot_caché et ajoute des espaces entre les lettres pour améliorer la lisibilité
         return ' '.join(mot_caché)
 
     
@@ -107,7 +107,7 @@ def jeu_du_pendu() :
     label_mot = tk.Label(jeu, text='')
     label_mot.pack(pady=100)
 
-    label_tentatives_restantes = tk.Label(jeu, text='Nombre de tentatives restantes: 7')
+    label_tentatives_restantes = tk.Label(jeu, text='Nombre de tentatives restantes:' + str(number))
     label_tentatives_restantes.pack(pady=50)
 
     entry_lettre = tk.Entry(jeu)
@@ -124,21 +124,21 @@ def jeu_du_pendu() :
     
     def deviner_une_lettre() :                           #fonction qui gère une tentative "gagnante" ou "perdante" 
         global tentatives_restantes, mot_caché
-        lettre = entry_lettre.get().lower()
-        entry_lettre.delete(0, tk.END)
-        if lettre in mot :
-            mot_caché = vérification(lettre, mot, mot_caché)
-            label_mot.config(text=montrer_mot_caché(mot_caché))
-            if '*' not in mot_caché :
-                label_tentatives_restantes.config(text='Vous avez gagné !')
-                bouton_deviner.config(state=tk.DISABLED)
-        else:
-            tentatives_restantes -= 1
-            label_tentatives_restantes.config(text=f'Nombre de tentatives restantes: {tentatives_restantes}')
-            if tentatives_restantes == 0:
-                label_mot.config(text=mot)
-                label_tentatives_restantes.config(text='Vous avez perdu !')
-                bouton_deviner.config(state=tk.DISABLED)
+        lettre = entry_lettre.get().lower()              #récupère entry_lettre et la stocke dans la variable lettre; .lower() pour mettre les lettres en minuscule pour éviter des erreurs
+        entry_lettre.delete(0, tk.END)                   #efface le contenu de entry_lettre pour permettre une nouvelle entrée
+        if lettre in mot :                               #boucle if qui vérifie si la lettre entrée est dans le mot
+            mot_caché = vérification(lettre, mot, mot_caché)        #si lettre dans le mot : appelle la fonction vérification  et met a jour mot_caché en remplacant les étoiles par la lettre trouvée
+            label_mot.config(text=montrer_mot_caché(mot_caché))     #met a jour le label mot en utilisant la fonction montrer_mot_caché
+            if '*' not in mot_caché :                    #vérifie qu'il n'y a plus d'étoiles donc de lettres a trouver pour gagner
+                label_tentatives_restantes.config(text='Vous avez gagné !')    #si aucune étoile : label des tentatives_restantes est mis a jour
+                bouton_deviner.config(state=tk.DISABLED)                       #le bouton deviner est désactivé car la partie est finie
+        else:                                           #si la lettre entrée n'est pas dans le mot :
+            tentatives_restantes -= 1                   #le joueur perd une tentative
+            label_tentatives_restantes.config(text=f'Nombre de tentatives restantes: {tentatives_restantes}') #met a jout le nombre de tentatives qu'il reste au joueur
+            if tentatives_restantes == 0:               #si le joueur n'a plus aucune tentative restante :
+                label_mot.config(text=mot)              #affiche le mot qui était a deviner si le joueur a perdu
+                label_tentatives_restantes.config(text='Vous avez perdu !')         #indique que le joueur a perdu
+                bouton_deviner.config(state=tk.DISABLED)            #le bouton deviner est désactivé, il ne reste plus qu'a rejouer ou quitter
 
     #def rejouer() :
         #global mot, mot_caché, tentatives_restantes
@@ -171,8 +171,10 @@ def jeu_du_pendu() :
 def jeu_du_pendu_aléatoire() :
 
     def choix_du_mot() :
-        mots_aléatoire = ['manger', 'test', 'manger', 'docteur', 'raton', 'effacer']
+        mots_aléatoire = ['manger', 'docteur', 'effacer', 'xylophone', 'guitare']
         return random.choice(mots_aléatoire)
+    
+    
 
     def vérification(lettre, mot, mot_caché) :           #vérifie si la lettre est dans le mot
         for i in range(len(mot)) :
@@ -206,6 +208,7 @@ def jeu_du_pendu_aléatoire() :
     mot_caché = '*' * len(mot)
     tentatives_restantes = 7
     
+
     def deviner_une_lettre() :                           #fonction qui gère une tentative "gagnante" ou "perdante" 
         global tentatives_restantes, mot_caché
         lettre = entry_lettre.get().lower()
@@ -223,6 +226,43 @@ def jeu_du_pendu_aléatoire() :
                 label_mot.config(text=mot)
                 label_tentatives_restantes.config(text='Vous avez perdu !')
                 bouton_deviner.config(state=tk.DISABLED)
+    def dessin_pendu():
+        canvas = Canvas(jeu, width=Image_1.width(), height=Image_2.height())
+        canvas.pack(side= LEFT)
+        Image_1= Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDU/imagependu0.png")
+        Image_2= Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDU\imagependu1.png")
+        Image_3= Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDUimagependu2.png")
+        Image_4= Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDUimagependu3.png")
+        Image_5=Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDU\imagependu4.png")
+        Image_6= Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDU\imagependu5.png")
+        Image_7= Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDU/imagependu6.png")
+        Image_8= Image.open("C:\Fac\L1 Semestre 2\IN200N\IMAGES PENDU/imagependu7.png")
+        photo1 = ImageTk.PhotoImage(Image_1)
+        photo2 = ImageTk.PhotoImage(Image_2)
+        photo3 = ImageTk.PhotoImage(Image_3)
+        photo4 = ImageTk.PhotoImage(Image_4)
+        photo5 = ImageTk.PhotoImage(Image_5)
+        photo6 = ImageTk.PhotoImage(Image_6)
+        photo7= ImageTk.PhotoImage(Image_7)
+        photo8= ImageTk.PhotoImage(Image_8)
+        if tentatives_restantes == 7 :
+            canvas.create_image(0, 0, anchor=NW, image=photo1)
+        if tentatives_restantes == 6 :
+            canvas.create_image(0, 0, anchor=NW, image=photo2)
+        if tentatives_restantes == 5 :
+            canvas.create_image(0, 0, anchor=NW, image=photo3)
+        if tentatives_restantes == 4 :
+            canvas.create_image(0, 0, anchor=NW, image=photo4)
+        if tentatives_restantes == 3 :
+            canvas.create_image(0, 0, anchor=NW, image=photo5)
+        if tentatives_restantes == 2 :
+            canvas.create_image(0, 0, anchor=NW, image=photo6)
+        if tentatives_restantes == 1 :
+            canvas.create_image(0, 0, anchor=NW, image=photo7)
+        if tentatives_restantes == 0 :
+            canvas.create_image(0, 0, anchor=NW, image=photo8)
+        
+        
 
     def rejouer() :
         jeu.destroy()
